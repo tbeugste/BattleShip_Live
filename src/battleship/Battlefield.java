61e342dc2bc458af40109996be0abe8fe7568702
pbuholzer <patrik.buholzer@stud.hslu.ch>
2013-12-06 10:43:08 +0100

    Battlefield und Buttonlistener

---
+++ src/battleship/Battlefield.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship;
+
+/**
+ *
+ * @author Patrik
+ */
+public class Battlefield {
+    
+    private int height;
+    private int width;
+    
+    public Battlefield (int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+    
+    public void setButtons () {
+        for (int i = 0; i < this.height; i++) {
+            for (int j = 0; j < this.width; j++) {
+                
+            }
+        }
+    }
+    
+    
+    
+}
