02a1d15d1635c421c58493ffd2d84437c9cc14b4
Andy <Andy@10.3.118.49>
2013-12-13 11:54:24 +0100

    Serveraufteilung für KI

--- src/battleship/engine/Shiptypes.java
+++ src/battleship/engine/Shiptypes.java
@@ -3,19 +3,22 @@
  * and open the template in the editor.
  */
 package battleship.engine;
+import java.awt.Point;
 import java.io.*;
 /**
  *
  * @author Andreas Eugster
  */
 public enum Shiptypes implements Serializable{
-    SUBMARINE(2,4), DESTROYER(3,3), CRUISER(4,2), BSHIP(5,1);
+    SUBMARINE(2,4, null), DESTROYER(3,3, null), CRUISER(4,2, null), BSHIP(5,1, null);
     
     private final int id;
     private final int count;
-    
-    Shiptypes(int id, int count) { this.id = id; this.count = count;}
+    private Point pos;
+    Shiptypes(int id, int count, Point pos) { this.id = id; this.count = count; this.pos = pos;}
     public int getValue() { return id; }
     public int getCount(){return count;}
+    public Point getPos(){return pos;}
+    public void setPos(Point pos){this.pos = pos;}
     
 }
