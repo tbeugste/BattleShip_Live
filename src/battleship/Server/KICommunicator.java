b455217daa34ba9bcdd81668c21c7cef6c0f654a
Andy <Andy@192.168.1.39>
2013-12-16 01:29:21 +0100

    KIcommunicator Shipcreator erstellt

--- src/battleship/Server/KICommunicator.java
+++ src/battleship/Server/KICommunicator.java
@@ -6,7 +6,9 @@
 
 package battleship.Server;
 import battleship.engine.*;
+import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Random;
 /**
  * This class is responsible to interact with the player. It moves and acts like one.
  * @author Andy
@@ -47,24 +49,115 @@ public class KICommunicator {
     * Method to create Ships on the battlefield
     */
     private void setRandomShips()
-    {}
+    {
+        _fleet.clear();
+        Ship ship;
+        ArrayList<Point> shipPos;
+        boolean horizontal;
+        Random rand = new Random();
+        for(Shiptypes type: Shiptypes.values())
+        {
+            for(int i = 0;i<type.getCount();i++)
+            {
+                do{
+                horizontal = rand.nextBoolean();
+                shipPos = getShipPos(type, horizontal);
+                ship = new Ship(shipPos, type);
+                }
+                while(!correctPlacement(ship));
+                _fleet.add(ship);
+            }
+        }
+    }
      
+    /**
+     * Method to create a new Shipposition
+     * @param type
+     * @param horizontal
+     * @return 
+     */
+    private ArrayList<Point> getShipPos(Shiptypes type, boolean horizontal)
+    {
+        ArrayList<Point> allPos = new ArrayList<>();
+        Point pos = new Point();
+        Random rand = new Random();
+        if(horizontal)
+        {
+            pos.x = rand.nextInt(10-type.getValue());
+            pos.y = rand.nextInt(10);
+            allPos.add(pos);
+            for(int i =0;i<type.getCount();i++)
+            {
+                pos.x=allPos.get(i).x+i;
+                pos.y=allPos.get(i).y;
+                allPos.add(pos);
+            }
+        }
+        else
+        {
+            pos.x = rand.nextInt(10);
+            pos.y = rand.nextInt(10-type.getValue());
+            allPos.add(pos);
+            for(int i =0;i<type.getCount();i++)
+            {
+                pos.x=allPos.get(i).x;
+                pos.y=allPos.get(i).y+i;
+                allPos.add(pos);
+            }
+        }
+        
+        return allPos;
+    }
+    
+    /**
+     * Method to test if the placement is correct
+     * @param tShip
+     * @return 
+     */
+    private boolean correctPlacement(Ship tShip)
+    {
+        boolean correct = true;
+        
+        for(Ship ship: _fleet)
+        {
+            for(Point shipPos: ship.getCoordinates())
+            {
+                for(Point tShipPos: tShip.getCoordinates())
+                {
+                    if(tShipPos.distance(shipPos)<=1)
+                    {
+                        correct=false;
+                        return correct;
+                    }
+                }
+            }
+        }
+        
+        return correct;
+        
+    }
     /**
       * Method to reset all local variables
       */
-    private void resetLocalVariables(){}
+    private void resetLocalVariables()
+    {
+        _fleet.clear();
+    }
     
     /**
      * Method to reset the game. Set new ships and reset all local variables
      */
     public void regame()
-    {}
+    {
+        resetLocalVariables();
+        setRandomShips();
+    }
     
     /**
      * apply Message to the KI
      * @param message 
      */
-    public CommunicationObject applyMessage(CommunicationObject message){return message;}
+    public CommunicationObject sendMessage(CommunicationObject message){return message;}
     
     private CommunicationObject shotMessage(CommunicationObject message){return message;}
     
